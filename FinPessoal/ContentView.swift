4b91ccaa2d579ef6c6a5b120815a2d1839615c6c
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2025-08-02 10:32:51 -0300

    Initial Commit

---
+++ FinPessoal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinPessoal
+//
+//  Created by Roberto Edgar Geiss on 02/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinPessoal/FinPessoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinPessoalApp.swift
+//  FinPessoal
+//
+//  Created by Roberto Edgar Geiss on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FinPessoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
