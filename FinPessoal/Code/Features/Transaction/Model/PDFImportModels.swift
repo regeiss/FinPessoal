184e3a390ff0c63dd2b89b1ffaaccc0c265cc991
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2026-02-22 21:02:24 -0300

    fix(pdf-import): add missing Equatable conformance and Combine import
    - Add Equatable conformance to PDFImportResult
    - Import Combine in PDFImportViewModel for @Published properties
    - Build now succeeds

--- FinPessoal/Code/Features/Transaction/Model/PDFImportModels.swift
+++ FinPessoal/Code/Features/Transaction/Model/PDFImportModels.swift
@@ -49,7 +49,7 @@ struct ParsedTransaction: Identifiable, Equatable {
 
 // MARK: - Import Result
 
-struct PDFImportResult {
+struct PDFImportResult: Equatable {
   let extracted: [ParsedTransaction]
   let duplicates: [ParsedTransaction]
   let errors: [PDFImportError]
--- FinPessoal/Code/Features/Transaction/ViewModel/PDFImportViewModel.swift
+++ FinPessoal/Code/Features/Transaction/ViewModel/PDFImportViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftUI
+import Combine
 import UniformTypeIdentifiers
 
 @MainActor
